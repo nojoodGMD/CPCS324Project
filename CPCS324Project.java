8ecf3b51a6726c239c0f98774b0a0cea5c1861f1
Reem <reemyziz@hotmail.com>
2021-11-07 11:47:58 +0300

    I edited main method

---
+++ CPCS324Project.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public class CPCS324Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public static Graph makeGraph(int vertex, int edge, int weight) {
+      
+    }
+    
+}
